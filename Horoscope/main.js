5bc90b0e827400ef5cc2d5f5df89d40964f2e87b
koulight <koulight@hotmail.com>
2022-01-22 00:00:04 -0800

    sailormoon style

--- Horoscope/main.js
+++ Horoscope/main.js
@@ -10,63 +10,98 @@ function checkDate(){
 
     if ((checkMonth === "december" && checkDay >= 21) || (checkMonth === "january" && checkDay <= 20)){
         sign.innerHTML = "Capricorn"
-        signInfo.innerHTML = "Info here"
+        signInfo.innerHTML = "Hotaru Tomoe"
+        document.querySelector("#capricorn").classList.remove("hidden")
+        showHide()
     }
         else if 
             ((checkMonth === "january" && checkDay >= 21) || (checkMonth === "february" && checkDay <= 18)){
                 sign.innerHTML = "Aquarius"
-                signInfo.innerHTML = "Info here"
+                signInfo.innerHTML = "Haruka Tenou"
+                document.querySelector("#aquarius").classList.remove("hidden")
+                showHide()
         }
         else if 
             ((checkMonth === "february" && checkDay >= 19) || (checkMonth === "march" && checkDay <= 20)){
                 sign.innerHTML = "Pisces"
-                signInfo.innerHTML = "Info here"
+                signInfo.innerHTML = "Michiru Kaiou"
+                document.querySelector("#pisces").classList.remove("hidden")
+                showHide()
         }
         else if 
             ((checkMonth === "march" && checkDay >= 21) || (checkMonth === "april" && checkDay <= 19)){
                 sign.innerHTML = "Aries"
-                signInfo.innerHTML = "Info here"
+                signInfo.innerHTML = "Rei Hino"
+                document.querySelector("#aries").classList.remove("hidden")
+                showHide()
         }
         else if 
             ((checkMonth === "april" && checkDay >= 20) || (checkMonth === "may" && checkDay <= 20)){
                 sign.innerHTML = "Taurus"
-                signInfo.innerHTML = "Info here"
+                signInfo.innerHTML = "Luna"
+                document.querySelector("#taurus").classList.remove("hidden")
+                showHide()
         }
         else if 
             ((checkMonth === "may" && checkDay >= 21) || (checkMonth === "june" && checkDay <= 20)){
                 sign.innerHTML = "Gemini"
-                signInfo.innerHTML = "Info here"
+                signInfo.innerHTML = "ChibiUsa"
+                document.querySelector("#gemini").classList.remove("hidden")
+                showHide()
         }
         else if 
             ((checkMonth === "june" && checkDay >= 21) || (checkMonth === "july" && checkDay <= 22)){
                 sign.innerHTML = "Cancer"
-                signInfo.innerHTML = "Info here"
+                signInfo.innerHTML = "Usagi Tsukino"
+                document.querySelector("#cancer").classList.remove("hidden")
+                showHide()
         }
         else if 
             ((checkMonth === "july" && checkDay >= 23) || (checkMonth === "august" && checkDay <= 22)){
                 sign.innerHTML = "Leo"
-                signInfo.innerHTML = "Info here"
+                signInfo.innerHTML = "Mamoru Chiba"
+                document.querySelector("#leo").classList.remove("hidden")
+                showHide()
         }
         else if 
             ((checkMonth === "august" && checkDay >= 23) || (checkMonth === "september" && checkDay <= 22)){
                 sign.innerHTML = "Virgo"
-                signInfo.innerHTML = "Info here"
+                signInfo.innerHTML = "Ami Mizuno"
+                document.querySelector("#virgo").classList.remove("hidden")
+                showHide()
         }
         else if 
             ((checkMonth === "september" && checkDay >= 23) || (checkMonth === "october" && checkDay <= 22)){
                 sign.innerHTML = "Libra"
-                signInfo.innerHTML = "Info here"
+                signInfo.innerHTML = "Aino Minako"
+                document.querySelector("#libra").classList.remove("hidden")
+                showHide()
         }
         else if 
             ((checkMonth === "october" && checkDay >= 23) || (checkMonth === "november" && checkDay <= 21)){
                 sign.innerHTML = "Scorpio"
-                signInfo.innerHTML = "Info here"
+                signInfo.innerHTML = "Setsuna Meiou"
+                document.querySelector("#scorpio").classList.remove("hidden")
+                showHide()
         }
         else if 
             ((checkMonth === "november" && checkDay >= 22) || (checkMonth === "december" && checkDay <= 21)){
                 sign.innerHTML = "Sagittarius"
-                signInfo.innerHTML = "Info here"
+                signInfo.innerHTML = "Makoto Kino"
+                document.querySelector("#sagittarius").classList.remove("hidden")
+                showHide()
         }
 
     }
+
+
+document.querySelector("#back").addEventListener("click", goBack)
+function goBack(){
+    window.location.reload()
+}
+
+function showHide(){
+    document.querySelector("section").classList.add("hidden")
+    document.querySelector("div").classList.remove("hidden")
+}
 
