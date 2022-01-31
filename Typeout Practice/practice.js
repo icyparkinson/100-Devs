73f1e4f2b03212d3e0d88b907a99983c47f43520
koulight <koulight@hotmail.com>
2022-01-30 16:05:03 -0800

    practice js

--- Typeout Practice/practice.js
+++ Typeout Practice/practice.js
@@ -507,24 +507,46 @@ let listOfNums = [1,2,3,4,5]
 //   return area
 // }
 
-let candy = ["mars", "oreos", "candy"]
-for (const sweets of candy){console.log(sweets)}
+// let candy = ["mars", "oreos", "candy"]
+// for (const sweets of candy){console.log(sweets)}
 
-function evenOnly(arr){
-  let newArray = []
-  for (let i = 0; i < arr.length; i++){
-    if (arr[i]%2===0){
-      newArray.push(arr[i])
-    }
-  } console.log(newArray)
+// function evenOnly(arr){
+//   let newArray = []
+//   for (let i = 0; i < arr.length; i++){
+//     if (arr[i]%2===0){
+//       newArray.push(arr[i])
+//     }
+//   } console.log(newArray)
+// }
+// let arr = [1,2,3,4,5]
+// evenOnly(arr)
+
+
+// let kwee = arr.sort((a,b) => a-b)
+
+// let woot = arr.reduce((difference, item) => {
+//   return difference - item},0)
+
+//   console.log(woot)
+
+
+function multNums(arr){
+  let product = arr.reduce((current, item) => current * item, 1)
+  console.log(product)
 }
-let arr = [1,2,3,4,5]
-evenOnly(arr)
+
+multNums([2,4,5])
 
 
-let kwee = arr.sort((a,b) => a-b)
+let nums = [1,2,3,4]
+nums.forEach((value, index) => console.log(value))
 
-let woot = arr.reduce((difference, item) => {
-  return difference - item},0)
 
-  console.log(woot)
+const sumNumbers = number => number + number
+
+function convertsReverse(str){
+  let reversed = str.split("").reverse().join("")
+  console.log(reversed)
+}
+
+convertsReverse("howdy")
